abe30c74705c022a7928076f9c94efb8dfa174d1
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-08-30 21:09:52 +0200

    add blazor hybrid project

---
+++ RockPaperScissors.BlazorHybrid.Client/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RockPaperScissors.BlazorHybrid.Client;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ RockPaperScissors.BlazorHybrid.Client/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RockPaperScissors.BlazorHybrid.Client;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
